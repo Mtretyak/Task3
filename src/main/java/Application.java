47942d64d487fbe68b7c6591ba26413326ff9a23
Mihail Tretyak <tretyak.mihail@mail.ru>
2017-02-10 19:37:18 +0300

    setup gradle

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -3,6 +3,7 @@ import java.net.*;
 import java.util.Scanner;
 
 public class Application {
+
     public static void main(String... args) {
         Scanner in = new Scanner(System.in);
         String input = in.nextLine();
